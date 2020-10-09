4912cfe84eb2b6cadb148a8037eae0453b6bf562
Sven Wappler <info@wappler.systems>
2020-10-09 21:00:36 +0200

    SendToAddressFinisher.php compatibility to form 10.

--- Classes/Domain/Form/Finishers/SendToAddressFinisher.php
+++ Classes/Domain/Form/Finishers/SendToAddressFinisher.php
@@ -2,35 +2,14 @@
 declare(strict_types=1);
 namespace WapplerSystems\Address\Domain\Form\Finishers;
 
-/*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
 
-use TYPO3\CMS\Core\Mail\MailMessage;
-use TYPO3\CMS\Extbase\Domain\Model\FileReference;
-use TYPO3\CMS\Fluid\View\StandaloneView;
-use TYPO3\CMS\Form\Domain\Finishers\AbstractFinisher;
+use Symfony\Component\Mime\Address;
+use TYPO3\CMS\Form\Domain\Finishers\EmailFinisher;
 use TYPO3\CMS\Form\Domain\Finishers\Exception\FinisherException;
-use TYPO3\CMS\Form\Domain\Model\FormElements\FileUpload;
-use TYPO3\CMS\Form\Domain\Runtime\FormRuntime;
-use TYPO3\CMS\Form\Service\TranslationService;
-use TYPO3\CMS\Form\ViewHelpers\RenderRenderableViewHelper;
 
 
-class SendToAddressFinisher extends AbstractFinisher
+class SendToAddressFinisher extends EmailFinisher
 {
-    const FORMAT_PLAINTEXT = 'plaintext';
-    const FORMAT_HTML = 'html';
-
 
     /**
      * @var \WapplerSystems\Address\Domain\Repository\AddressRepository
@@ -50,51 +29,26 @@ class SendToAddressFinisher extends AbstractFinisher
 
 
     /**
-     * @var array
-     */
-    protected $defaultOptions = [
-        'recipientName' => '',
-        'senderName' => '',
-        'format' => self::FORMAT_HTML,
-        'attachUploads' => true
-    ];
-
-    /**
-     * Executes this finisher
-     * @see AbstractFinisher::execute()
+     * Get mail recipients
      *
-     * @throws FinisherException
+     * @param string $listOption List option name
+     * @param string $singleAddressOption Single address option
+     * @param string|null $singleAddressNameOption Single address name
+     * @return array
+     *
+     * @deprecated since TYPO3 v10.0, will be removed in TYPO3 v11.0.
      */
-    protected function executeInternal()
-    {
-        $formRuntime = $this->finisherContext->getFormRuntime();
-        $standaloneView = $this->initializeStandaloneView($formRuntime);
-
-        $translationService = TranslationService::getInstance();
-        if (isset($this->options['translation']['language']) && !empty($this->options['translation']['language'])) {
-            $languageBackup = $translationService->getLanguage();
-            $translationService->setLanguage($this->options['translation']['language']);
-        }
-        $message = $standaloneView->render();
-        if (!empty($languageBackup)) {
-            $translationService->setLanguage($languageBackup);
-        }
-
-        $subject = $this->parseOption('subject');
-        $senderAddress = $this->parseOption('senderAddress');
-        $senderName = $this->parseOption('senderName');
-        $replyToAddress = $this->parseOption('replyToAddress');
-        $carbonCopyAddress = $this->parseOption('carbonCopyAddress');
-        $blindCarbonCopyAddress = $this->parseOption('blindCarbonCopyAddress');
-        $format = $this->parseOption('format');
-        $attachUploads = $this->parseOption('attachUploads');
+    protected function getRecipients(
+        string $listOption,
+        string $singleAddressOption,
+        string $singleAddressNameOption = null
+    ): array {
 
         $values = $this->finisherContext->getFormValues();
 
         if ((int)$values['addressUid'] === 0) {
             throw new FinisherException('No address given.', 132706567666);
         }
-
 
         $address = $this->addressRepository->findByUid($values['addressUid']);
         if ($address === null) {
@@ -104,110 +58,9 @@ class SendToAddressFinisher extends AbstractFinisher
         $recipientAddress = $address->getEmail();
         $recipientName = $address->getName();
 
-
-        if (empty($subject)) {
-            throw new FinisherException('The option "subject" must be set for the EmailFinisher.', 1327060320);
-        }
-        if (empty($recipientAddress)) {
-            throw new FinisherException('The option "recipientAddress" must be set for the EmailFinisher.', 1327060200);
-        }
-        if (empty($senderAddress)) {
-            throw new FinisherException('The option "senderAddress" must be set for the EmailFinisher.', 1327060210);
-        }
-
-        $mail = $this->objectManager->get(MailMessage::class);
-
-        $mail->setFrom([$senderAddress => $senderName])
-            ->setTo([$recipientAddress => $recipientName])
-            ->setSubject($subject);
-
-        if (!empty($values['email'])) {
-            $mail->setReplyTo($values['email']);
-        }
-
-        if (!empty($carbonCopyAddress)) {
-            $mail->setCc($carbonCopyAddress);
-        }
-
-        if (!empty($blindCarbonCopyAddress)) {
-            $mail->setBcc($blindCarbonCopyAddress);
-        }
-
-        if ($format === self::FORMAT_PLAINTEXT) {
-            $mail->setBody($message, 'text/plain');
-        } else {
-            $mail->setBody($message, 'text/html');
-        }
-
-        $elements = $formRuntime->getFormDefinition()->getRenderablesRecursively();
-
-        if ($attachUploads) {
-            foreach ($elements as $element) {
-                if (!$element instanceof FileUpload) {
-                    continue;
-                }
-                $file = $formRuntime[$element->getIdentifier()];
-                if ($file) {
-                    if ($file instanceof FileReference) {
-                        $file = $file->getOriginalResource();
-                    }
-
-                    $mail->attach(\Swift_Attachment::newInstance($file->getContents(), $file->getName(), $file->getMimeType()));
-                }
-            }
-        }
-
-        $mail->send();
+        $addresses = [];
+        $addresses[] = new Address($recipientAddress, $recipientName);
+        return $addresses;
     }
 
-    /**
-     * @param FormRuntime $formRuntime
-     * @return StandaloneView
-     * @throws FinisherException
-     */
-    protected function initializeStandaloneView(FormRuntime $formRuntime): StandaloneView
-    {
-        $format = ucfirst($this->parseOption('format'));
-        $standaloneView = $this->objectManager->get(StandaloneView::class);
-
-        if (isset($this->options['templatePathAndFilename'])) {
-            $this->options['templatePathAndFilename'] = strtr($this->options['templatePathAndFilename'], [
-                '{@format}' => $format
-            ]);
-            $standaloneView->setTemplatePathAndFilename($this->options['templatePathAndFilename']);
-        } else {
-            if (!isset($this->options['templateName'])) {
-                throw new FinisherException('The option "templateName" must be set for the EmailFinisher.', 1327058829);
-            }
-            $this->options['templateName'] = strtr($this->options['templateName'], [
-                '{@format}' => $format
-            ]);
-            $standaloneView->setTemplate($this->options['templateName']);
-        }
-
-        $standaloneView->assign('finisherVariableProvider', $this->finisherContext->getFinisherVariableProvider());
-
-        if (isset($this->options['templateRootPaths']) && is_array($this->options['templateRootPaths'])) {
-            $standaloneView->setTemplateRootPaths($this->options['templateRootPaths']);
-        }
-
-        if (isset($this->options['partialRootPaths']) && is_array($this->options['partialRootPaths'])) {
-            $standaloneView->setPartialRootPaths($this->options['partialRootPaths']);
-        }
-
-        if (isset($this->options['layoutRootPaths']) && is_array($this->options['layoutRootPaths'])) {
-            $standaloneView->setLayoutRootPaths($this->options['layoutRootPaths']);
-        }
-
-        if (isset($this->options['variables'])) {
-            $standaloneView->assignMultiple($this->options['variables']);
-        }
-
-        $standaloneView->assign('form', $formRuntime);
-        $standaloneView->getRenderingContext()
-            ->getViewHelperVariableContainer()
-            ->addOrUpdate(RenderRenderableViewHelper::class, 'formRuntime', $formRuntime);
-
-        return $standaloneView;
-    }
 }
