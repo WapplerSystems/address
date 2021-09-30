521667bbc2493f3ae9da2a9448dba5d6a76a2ba1
Sven Wappler <info@wappler.systems>
2021-09-30 12:36:47 +0200

    map fixes, coding styles, google maps update

--- Classes/Map/Encoder.php
+++ Classes/Map/Encoder.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace WapplerSystems\Address\Map;
 
 /**
@@ -10,32 +11,34 @@ namespace WapplerSystems\Address\Map;
 
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
-class Encoder {
-	
-	
-	private $settings = array();
+class Encoder
+{
 
-	public function __construct($settings) {
-		$this->settings = $settings;
-		
-	}
+    private $settings = [];
+
+    public function __construct($settings)
+    {
+        $this->settings = $settings;
+
+    }
 
 
     /**
      * @param $addressQuery
      * @return null|string
      */
-    public function getCoordinatesByAddress($addressQuery) {
-		$coordinate = null;
-		$geocodeUrl = $this->settings['googlemaps']['geocode']['apiUrl'];
-		$geocodeUrl .= '?sensor=false&address=' . urlencode(str_replace(LF, ', ', $addressQuery));
-		$geocodeResult = GeneralUtility::getURL($geocodeUrl);
-		$geocodeResult = json_decode($geocodeResult);
-		if ($geocodeResult !== null && strtolower($geocodeResult->status) === 'ok') {
-			$coordinate = $geocodeResult->results[0]->geometry->location->lat . ',' . $geocodeResult->results[0]->geometry->location->lng;
-		}
-		return $coordinate;
-	}
+    public function getCoordinatesByAddress($addressQuery)
+    {
+        $coordinate = null;
+        $geocodeUrl = $this->settings['googlemaps']['geocode']['apiUrl'];
+        $geocodeUrl .= '?sensor=false&address=' . urlencode(str_replace(LF, ', ', $addressQuery));
+        $geocodeResult = GeneralUtility::getURL($geocodeUrl);
+        $geocodeResult = json_decode($geocodeResult);
+        if ($geocodeResult !== null && strtolower($geocodeResult->status) === 'ok') {
+            $coordinate = $geocodeResult->results[0]->geometry->location->lat . ',' . $geocodeResult->results[0]->geometry->location->lng;
+        }
+        return $coordinate;
+    }
 
 
     /**
@@ -43,25 +46,25 @@ class Encoder {
      * @param $countryCode
      * @return null|LatLng
      */
-    public function getLatLngByAddress($address,$countryCode) {
-		$latLng = null;
-		$geocodeUrl = $this->settings['googlemaps']['geocode']['apiUrl'];
-		$geocodeUrl .= '?sensor=false&address=' . urlencode(str_replace(LF, ', ', $address));
+    public function getLatLngByAddress($address, $countryCode)
+    {
+        $latLng = null;
+        $geocodeUrl = $this->settings['googlemaps']['geocode']['apiUrl'];
+        $geocodeUrl .= '?sensor=false&address=' . urlencode(str_replace(LF, ', ', $address));
         if ($countryCode) {
-            $geocodeUrl .= '&components=country:' .$countryCode;
+            $geocodeUrl .= '&components=country:' . $countryCode;
         }
-		$geocodeResult = GeneralUtility::getURL($geocodeUrl);
-		$geocodeResult = json_decode($geocodeResult);
-		
-		if ($geocodeResult !== null && strtolower($geocodeResult->status) === 'ok') {
-			
-			$latLng = new LatLng($geocodeResult->results[0]->geometry->location->lat, $geocodeResult->results[0]->geometry->location->lng);
-		} else {
-			throw new \RuntimeException('no result');
-		}
-		return $latLng;
-	}
-	
+        $geocodeResult = GeneralUtility::getURL($geocodeUrl);
+        $geocodeResult = json_decode($geocodeResult);
 
-	
+        if ($geocodeResult !== null && strtolower($geocodeResult->status) === 'ok') {
+
+            $latLng = new LatLng($geocodeResult->results[0]->geometry->location->lat, $geocodeResult->results[0]->geometry->location->lng);
+        } else {
+            throw new \RuntimeException('no result');
+        }
+        return $latLng;
+    }
+
+
 }
