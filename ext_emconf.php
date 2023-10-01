0fa679d01956ab409ae6891aa399c01b716686d4
Sven Wappler <info@wappler.systems>
2023-10-02 01:24:04 +0200

    add ext_emconf.php for TER

---
+++ ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$EM_CONF['address'] = [
+    'title' => 'Address list',
+    'description' => 'Address extension with multiple contact types per address',
+    'category' => 'fe',
+    'author' => 'Sven Wappler',
+    'author_email' => 'typo3YYYY@wappler.systems',
+    'state' => 'stable',
+    'clearCacheOnLoad' => 1,
+    'author_company' => 'WapplerSystems',
+    'version' => '12.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.4.0-12.4.99',
+        ],
+    ],
+];
