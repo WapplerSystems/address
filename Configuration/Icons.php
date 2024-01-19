37f1bd2c5a6f13bb257b7a545fb1f4d293fbf7bd
Sven Wappler <info@wappler.systems>
2024-01-19 11:38:55 +0100

    new contact types and icons

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -27,9 +27,45 @@ return [
         'provider' => SvgIconProvider::class,
         'source' => 'EXT:address/Resources/Public/Icons/address_domain_model_content_email.svg',
     ],
+    'ext-address-contact-type-x' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:address/Resources/Public/Icons/address_domain_model_content_x.svg',
+    ],
     'ext-address-contact-type-mobilephone' => [
         'provider' => SvgIconProvider::class,
         'source' => 'EXT:address/Resources/Public/Icons/address_domain_model_content_mobilephone.svg',
+    ],
+    'ext-address-contact-type-website' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:address/Resources/Public/Icons/address_domain_model_content_website.svg',
+    ],
+    'ext-address-contact-type-fax' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:address/Resources/Public/Icons/address_domain_model_content_fax.svg',
+    ],
+    'ext-address-contact-type-threema' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:address/Resources/Public/Icons/address_domain_model_content_unknown.svg',
+    ],
+    'ext-address-contact-type-linkedin' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:address/Resources/Public/Icons/address_domain_model_content_linkedin.svg',
+    ],
+    'ext-address-contact-type-instagram' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:address/Resources/Public/Icons/address_domain_model_content_instagram.svg',
+    ],
+    'ext-address-contact-type-vimeo' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:address/Resources/Public/Icons/address_domain_model_content_vimeo.svg',
+    ],
+    'ext-address-contact-type-twitch' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:address/Resources/Public/Icons/address_domain_model_content_twitch.svg',
+    ],
+    'ext-address-contact-type-xing' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:address/Resources/Public/Icons/address_domain_model_content_xing.svg',
     ],
     'ext-address-plugin-list-and-detail' => [
         'provider' => SvgIconProvider::class,
