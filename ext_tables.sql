eb4b6689e6a574f7538d379b8fac37f358faa19a
Sven Wappler <wappler@wappler.systems>
2024-12-20 12:55:06 +0100

    remove default sql declarations

--- ext_tables.sql
+++ ext_tables.sql
@@ -3,21 +3,6 @@
 #
 CREATE TABLE tx_address_domain_model_address
 (
-	uid                   int(11)                      NOT NULL auto_increment,
-	pid                   int(11)          DEFAULT '0' NOT NULL,
-	tstamp                int(11)          DEFAULT '0' NOT NULL,
-	crdate                int(11)          DEFAULT '0' NOT NULL,
-	cruser_id             int(11)          DEFAULT '0' NOT NULL,
-	t3_origuid            int(11)          DEFAULT '0' NOT NULL,
-	editlock              tinyint(4)       DEFAULT '0' NOT NULL,
-	sys_language_uid      int(11)          DEFAULT '0' NOT NULL,
-	l10n_parent           int(11)          DEFAULT '0' NOT NULL,
-	l10n_diffsource       mediumtext,
-	deleted               tinyint(4)       DEFAULT '0' NOT NULL,
-	hidden                tinyint(4)       DEFAULT '0' NOT NULL,
-	starttime             int(11)          DEFAULT '0' NOT NULL,
-	endtime               int(11)          DEFAULT '0' NOT NULL,
-	sorting               int(11)          DEFAULT '0' NOT NULL,
 
 	title                 tinytext,
 	first_name            tinytext,
@@ -77,43 +62,18 @@ CREATE TABLE tx_address_domain_model_address
 	import_id             varchar(100)     DEFAULT ''  NOT NULL,
 	import_source         varchar(100)     DEFAULT ''  NOT NULL,
 
-
-	PRIMARY KEY (uid),
-	KEY parent (pid),
-	KEY sys_language_uid_l10n_parent (sys_language_uid, l10n_parent),
-	KEY import (import_id, import_source)
+	inline_map            varchar(255)     DEFAULT ''
 );
 
 
 CREATE TABLE tx_address_domain_model_contact
 (
-	uid              int(11)                NOT NULL auto_increment,
-	pid              int(11)    DEFAULT '0' NOT NULL,
-	tstamp           int(11)    DEFAULT '0' NOT NULL,
-	crdate           int(11)    DEFAULT '0' NOT NULL,
-	cruser_id        int(11)    DEFAULT '0' NOT NULL,
-	t3_origuid       int(11)    DEFAULT '0' NOT NULL,
-	editlock         tinyint(4) DEFAULT '0' NOT NULL,
-	sys_language_uid int(11)    DEFAULT '0' NOT NULL,
-	l10n_parent      int(11)    DEFAULT '0' NOT NULL,
-	l10n_diffsource  mediumtext,
-	deleted          tinyint(4) DEFAULT '0' NOT NULL,
-	hidden           tinyint(4) DEFAULT '0' NOT NULL,
-	starttime        int(11)    DEFAULT '0' NOT NULL,
-	endtime          int(11)    DEFAULT '0' NOT NULL,
-	sorting          int(11)    DEFAULT '0' NOT NULL,
-
 	type             varchar(100)           NOT NULL DEFAULT '',
 	content          text,
 
 	address          int(11)    DEFAULT '0' NOT NULL,
 
-	notes            text,
-
-
-	PRIMARY KEY (uid),
-	KEY parent (pid),
-	KEY sys_language_uid_l10n_parent (sys_language_uid, l10n_parent)
+	notes            text
 );
 
 
