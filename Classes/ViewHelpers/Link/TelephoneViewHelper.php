1b44ef5abe6d038b8b2f0624369673d164fa19fb
Sven Wappler <wappler@wappler.systems>
2025-08-08 01:44:42 +0200

    Add TelephoneViewHelper for rendering telephone links

---
+++ Classes/ViewHelpers/Link/TelephoneViewHelper.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace WapplerSystems\Address\ViewHelpers\Link;
+
+
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper;
+
+
+class TelephoneViewHelper extends AbstractTagBasedViewHelper
+{
+
+    protected $tagName = 'a';
+
+    public function initializeArguments()
+    {
+        parent::initializeArguments();
+        $this->registerArgument('number', 'string', 'Telephone number', true);
+    }
+
+    public function render(): string
+    {
+        $number = $this->arguments['number'] ?? '';
+        if ($number === '' || !str_starts_with($number, '+')) {
+            return (string)$this->renderChildren();
+        }
+
+        $number = str_replace('(0)', '', $number);
+        // Remove any non-numeric characters from the number
+        $number = preg_replace('/[^\d+]/', '', $number);
+
+        $this->tag->addAttribute('href', 'tel:' . $number);
+        $this->tag->setContent((string)$this->renderChildren());
+        $this->tag->forceClosingTag(true);
+        return $this->tag->render();
+    }
+
+}
